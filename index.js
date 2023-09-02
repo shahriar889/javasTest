03bc3f9c39eae86a2f4e1718e177af81b490407a
Khandaker Shahriar Rahman <ksrahman@sfu.ca>
2023-09-02 02:00:18 -0700

    Added mypage.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 8000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
